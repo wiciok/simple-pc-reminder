40470095c835f6651ec30349c212a6115cbc9e10
matimac1994 <matiwsrh1994@gmail.com>
2016-05-24 17:34:50 +0200

    raz

---
+++ src/main/java/logic/Scheduler.java
@@ -0,0 +1,12 @@
+package logic;
+
+/**
+ * Created by Witek on 2016-05-22.
+ * tu najpewniej będzie jakiś scheduler, ale to wyjdzie w praniu
+ */
+public class Scheduler
+{
+
+
+
+}
---
+++ src/main/java/testinterface/TestUserInterface.java
@@ -0,0 +1,15 @@
+package testinterface;
+
+/**
+ * Created by Witek on 2016-05-22.
+ * tutaj bedzie tymczasowy tekstowy interfejs do testowania logiki programu
+ */
+public class TestUserInterface
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("test");
+    }
+
+}
