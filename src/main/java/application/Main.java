d61147eeb10f4ded2406626b30de10eb67ca414b
pakopako <pawciopako@gmail.com>
2016-05-24 23:04:30 +0200

    Pawel: poprawki do wczesniejszego commitu - zrobienie wszystkiego w klasie Interface

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,8 +5,6 @@
 
 package application;
 	
-import java.io.IOException;
-
 import data.Database;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -32,33 +30,5 @@ public class Main extends Application {
 	public static void main(String[] args)
 	{
 		launch(args);
-		
-		Database testDatabase = new Database();
-		testDatabase.addToList(new Event());
-		testDatabase.addToList(new Event());
-		testDatabase.addToList(new Event());
-		testDatabase.addToList(new Event());
-		
-		System.out.println("List size after adding events:"+testDatabase.returnSize());
-		
-		try
-		{
-			testDatabase.writeToFile();
-		}
-		catch (IOException e)
-		{
-			System.out.println("Error writing to file: IOException (try/catch block in main)");
-			e.printStackTrace();
-		}
-		
-		try
-		{
-			testDatabase.readFromFile();
-		}
-		catch (IOException e)
-		{
-			System.out.println("Error reading from file: IOException (try/catch block in main)");
-			e.printStackTrace();
-		}
 	}
 }
