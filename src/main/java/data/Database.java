a69612dfbb19cbb8dcad33b362885e94d2215943
wiciok <wiciok1@gmail.com>
2016-05-30 20:40:09 +0200

    -poprawione kodowanie WAŻNE: pliki zapisujemy w kodowaniu UTF-8, a nie win-1250, bo wsystko się potem pieprzy!!!
    Event:
    -zmiana nazw niektórych pól i metod na poprawne językowo
    -zmiana typów zmiennych i modyfikacje getterów - wrappery, jak np. Integer, czy Boolean są immutable! Zmieniłem je na typy podstawowe i zmodyfikowałem gettery.
    -powiązana z powyższym modyfikacja setterów - proste przypisanie, zamiast alokacji nowych obiektów do starych referencji, ponadto ujednolica to całość w kontekście pól boolean (a nie Boolean)
    -wywalone niepotrzebne inicjalizacje pól, skoro przypisania są zaimplementowane w każdym konstruktorze
    -info o tym, co trzeba zrobić - głębokie kopiowanie w metodzie clone(), zwracanie całej daty z czasem
    -wyjebana metoda print
    
    Database:
    -wypierdolone chiskie kombinacje w metodzie add - odpowiada teraz standardowym interfejson javy
    -dodane zapisywanie i wczytywanie z pliku ilości rekordów
    -wypierdolona klasa display
    -zamiast display dorobiona metoda get
    
    TestUserInterface:
    -poprawki dot. zmienionych w inncyh klasach metod

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -3,7 +3,6 @@ package data;
 import logic.event.*;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -14,72 +13,51 @@ import java.io.ObjectOutputStream;
 
 /**
  * Implemented by Pawel
+ *Klasa ktora jest warstwa danych. Jej zadaniem jest obsluga bazy wydarzen.
  *
- *Na wstepie: klasa Event implementuje interfejs Serializable - jest to potrzebne do
- *zapisu i odczytu binarnego do/z pliku i nie wiem jak to robic bez tego (i czy sie da bez tego)
- *wiec nie usuwajcie tego jak mozecie :D
- *
- *Klasa oktora jest warstwa logiczna. Jej zadaniem jest obsluga bazy wydarzen.
- *Narazie ma 5 metod: addToList, displayEventList, writeToFile, readFromFile, returnSize 
- *i liste z wydarzeniami: databaseList. Opisy poszczegolnych funkcji przy deklaracji.
- *
- *TODO: OGARNAC JAK SPRAWDZAC W PLIKU ILE JEST ZAPISANYCH OBIEKTOW
- *		ZEBY TEJ TABLICY NIE MUSIEC CIAGLE SZTYWNO ZMIENIAC W KODZIE (readFromFile)
- * 
- * 		WYSWIETLANIE LISTY - MUSI BYC COS W KLASIE EVENT
- *
- *
- *
- *
+ * ToDo: dorobić sortowanie po dacie rozpoczęcia wydarzenia (najpierw musi istnieć w klasie Event metoda zwracająca jednocześnie date i czas
  */
 
 public class Database
 {
 	private ArrayList<Event> databaseList = new ArrayList<Event> ();
-	
-/**
- *addToList - metoda pozwalajaca do dodawania elementow do listy wydarzen (databaseList)
- *uzytkownik moze dodac na koniec listy lub wybrac sobie miejsce na ktore chce wstawic
- *Zalezy co zostale wybrane w menu - jesli 1 to event jest dodany na koniec,
- *jesli 2 to wevent jest dodany na miejsce wskazane przez uzytkownika (o ile zostanie zachowana ciaglosc listy)
- **/
-	public void addToList(Event newEvent, Integer addOption, Integer index)
-	{
-		if(index == null)
-			index = databaseList.size();
-		
-		switch(addOption)
-		{
-			case 1:	databaseList.add(newEvent);
-					break;
-						
-			case 2: databaseList.add(index, newEvent);
-					break;
-		}
-	}
-	
+
 	/**
-	*displayEventList - metoda wyswietlajaca wydarzenia - widze to tak ze tez uzytkownik moze sobie wyswietlic 1 wydarzenie, wszystkie, lub jakies konkretne
-	*ale do tego w klasie Event musi juz cos byc(przede wszystkim jakas funkcja wyswietlajaca wydarzenie ktora tu wywolam)
-	**/
-	public void displayEventList()
+	 *add - metoda pozwalajaca do dodawania elementow do listy wydarzen (databaseList)
+	 *wywołuje metodę add składnika ArrayList<Event> databaseList</>
+	 **/
+	public void add(Integer index, Event newEvent)
 	{
-		for(int i =0; i<databaseList.size(); i++)
-		{
-			//TODO:
-			//databaseList.get(i).jakasFunkcjaWypisujacaEvent;
-			
-			//zeby wyswietlac te wydarzenia w klasie Event musi byc jakas funkcja je wyswietlajaca
-			//i tu ja bedziemy wywolywac. Narazie jej nie ma wiec to zostawiam.
-		}
+		databaseList.add(index, newEvent);
 	}
+
+	public void add(Event newEvent)
+	{
+		databaseList.add(newEvent);
+	}
+
 	/**
-	*returnSize - zwraca ilosc wydarzen, taka funkcja zawsze sie przydaje w mainie - lista jest private
+	*size - zwraca ilosc wydarzen, taka funkcja zawsze sie przydaje w mainie - lista jest private
 	**/
-	public int returnSize() //przydaje sie
+	public int size()
 	{
 		return databaseList.size();
 	}
+
+	/**
+	 * get - zwraca obiekt Event z bazy - pierwszy, lub o podanym indeksie
+	 */
+
+	public Event get()
+	{
+		return databaseList.get(0);
+	}
+
+	public Event get(int index)
+	{
+		return databaseList.get(index);
+	}
+
 	
 	/**
 	*writeToFile - zapis do pliku binarnego
@@ -89,52 +67,57 @@ public class Database
 	public void writeToFile() throws IOException
 	{
 		String filePath = "binaryFile.dat";
-		ObjectOutputStream oos = null;
+		ObjectOutputStream file = null;
 		try
 		{
-			oos = new ObjectOutputStream(new FileOutputStream(filePath));
+			file = new ObjectOutputStream(new FileOutputStream(filePath));
 		}
 		catch (FileNotFoundException e)
 		{
-			System.out.println("Error File not found (writeToFile)");
-			e.printStackTrace();
+			//System.out.println("Error File not found (writeToFile)");
+			//e.printStackTrace();
+			throw new IOException(e.getCause()); //przekazywanie wyjatku wyżej, do interfejsu użytkownika
 		}
+
+		file.writeInt(databaseList.size()); //zapis ilości rekordów
 
 		for(int i = 0; i<databaseList.size(); ++i)
 		{
-			oos.writeObject(databaseList.get(i));
+			file.writeObject(databaseList.get(i));
 		}
-		oos.close();
+		file.close();
 	}
 	
 	/**
 	* readFromFile - odczyt z pliku binarnego.
-	* TODO jest opisane w funckji
 	* @throws IOException
 	**/
 	public void readFromFile() throws IOException
 	{
 		int i;
 		String filePath = "binaryFile.dat";
-		ObjectInputStream ois = null;
+		ObjectInputStream file = null;
 		try
 		{
-			ois = new ObjectInputStream(new FileInputStream(filePath));
+			file = new ObjectInputStream(new FileInputStream(filePath));
 		}
 		catch (FileNotFoundException e)
 		{
-			System.out.println("Error File not found (readFromFile)");
-			e.printStackTrace();
+			//System.out.println("Error File not found (readFromFile)");
+			//e.printStackTrace();
+			throw new IOException(e.getCause()); //przekazywanie wyjatku wyżej, do interfejsu użytkownika
 		}
-		//TODO: OGARNAC JAK SPRAWDZAC W PLIKU ILE JEST ZAPISANYCH OBIEKTOW
-		//ZEBY TEJ TABLICY NIE MUSIEC CIAGLE SZTYWNO ZMIENIAC W KODZIE
-		Event[] tmp = new Event[databaseList.size()];
-		
-		for(i = 0; i<tmp.length; i++) 
+
+
+		//TODO: dorobić tutaj przechwytywanie wyjątku, gdyby wczytywanie inta zakończyło się niepowodzeniem
+		final int size = file.readInt();
+		ArrayList<Event> tmp = new ArrayList<Event> ();
+
+		for(i = 0; i<size; i++)
 		{
 			try
 			{
-				tmp[i] = (Event) ois.readObject();
+				tmp.add( (Event)file.readObject());
 			}
 			catch (ClassNotFoundException e)
 			{
@@ -142,6 +125,9 @@ public class Database
 				e.printStackTrace();
 			}
 		}
-		ois.close();
+
+		databaseList.clear();
+		databaseList=tmp;
+		file.close();
 	}
 }
