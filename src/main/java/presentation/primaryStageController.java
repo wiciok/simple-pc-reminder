2fd437961460be8904f1022182a1e3ce204f7e65
wiciok <wiciok1@gmail.com>
2016-06-07 21:52:09 +0200

    -poczatek tworzenia gui

--- src/main/java/presentation/primaryStageController.java
+++ src/main/java/presentation/primaryStageController.java
@@ -7,22 +7,32 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import presentation.Main.*;
+
 /**
  *
  * @author Witek
  */
 public class primaryStageController implements Initializable
 {
-
-    @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-        System.out.println("You clicked me!");
-    }
-
     public void initialize(URL url, ResourceBundle rb)
     {
         // TODO
     }
+
+    @FXML
+    private void exampleHandleButtonAction(ActionEvent event)
+    {
+        System.out.println("You clicked me!");
+    }
+
+   /* @FXML
+    private void closeButtonAction(ActionEvent event)
+    {
+        primaryStage
+    }*/
+
+
+
 
 }
