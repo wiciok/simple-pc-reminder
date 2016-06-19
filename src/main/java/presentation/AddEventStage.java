38a95937854830c81bc9b7acdf702ef28c52a044
pakopako <pawciopako@gmail.com>
2016-06-19 10:16:04 +0200

    Paweł: implementacja addEvent, ale jest jakis blad - NullPointerException i nie wiem co jest nie tak.

--- src/main/java/presentation/AddEventStage.java
+++ src/main/java/presentation/AddEventStage.java
@@ -2,6 +2,8 @@ package presentation;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
@@ -13,7 +15,7 @@ public class AddEventStage
 {
     presentation.Main mainApp;
     Stage addEventStage;
-    private GridPane root;
+    private AnchorPane root; /*zmiana na AnchorPane*/
 
 
     AddEventStage(presentation.Main mainApp)
