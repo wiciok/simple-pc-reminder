89fdcc8facc973c91554f572e2d18f0bf12f5050
pakopako <pawciopako@gmail.com>
2016-06-22 10:45:20 +0200

    a

---
+++ src/main/java/controller/PropertiesStageController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import view.AddEventStage;
+import view.PropertiesStage;
+
+public class PropertiesStageController
+{
+	private PropertiesStage propertiesStage;
+	
+	public void setMainApp(PropertiesStage PropertiesStage)
+    {
+        this.propertiesStage = PropertiesStage;
+    }
+}
